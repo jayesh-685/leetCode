fee1cd04889b31718ffe091f4a44d487502461c6
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-06-13 14:13:28 +0530

    Update 120-triangle.cpp

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+//     bottom up dp approach
+//     create an array that stores the last level and start from second last level
+//     for each index calculate the shortest path value by getting the min of i and i+1 index of next level.
+//     since for each level we only need the level which is just below the current level we reuse the same array.
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         vector <int> dp = triangle[n-1];

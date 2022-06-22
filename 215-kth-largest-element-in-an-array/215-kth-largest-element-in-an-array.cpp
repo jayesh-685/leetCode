5b8ff7d275100903c25d7dc50edde61b12f62f4e
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-06-22 18:17:12 +0530

    Time: 346 ms (5.02%), Space: 10.1 MB (61.91%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue <int, vector<int>, greater<>> pq;
-        for (int i=0; i<k; i++)
-            pq.push(nums[i]);
-        
-        for (int i=k; i<nums.size(); i++) {
-            if (nums[i]>pq.top()) {
-                pq.pop();
-                pq.push(nums[i]);
+    
+//     based on quick sort
+//     if k (=length-k that is index of k largest element) is equal to pivot then we have found our answer else if it greater than pivot then we apply quick select for right part of pivot else for left part
+    int K;
+    int quickSelect (int l, int r, vector<int>& nums) {
+        int pivot=r, index=l;
+        for (int i=l; i<r; i++) {
+            if (nums[i]<=nums[pivot]) {
+                swap(nums[i], nums[index]);
+                index++;
             }
         }
         
-        return pq.top();
+        swap(nums[index], nums[pivot]);
+        
+        int length = nums.size();
+        if (length-K > index)       return quickSelect(index+1, r, nums);
+        else if (length-K<index)    return quickSelect(l, index-1, nums);
+        else                        return nums[index];
+    }
+    
+    int findKthLargest(vector<int>& nums, int k) {
+        K = k;
+        return quickSelect(0, nums.size()-1, nums);
     }
 };
+ 

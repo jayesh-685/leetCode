a690cdf0244a2507fb1ca31eecba2207109ad392
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-06-25 00:26:52 +0530

    Time: 82 ms (89.56%), Space: 27.5 MB (82.56%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n=mat.size(), m=mat[0].size();
-        vector <vector <int>> ans (n, vector <int> (m, INT_MAX));
-        queue <pair<int, int>> q;
-        
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
-                if (!mat[i][j]) {
-                    ans[i][j] = 0;
-                    q.push({i, j});
+//     dp solution
+//     if element is not 0 then its distance is minimum of its 4 neighbours +1
+//     if we traverse from top-left to bottom-right then for each element we have calculated distance of top and left neighbour so we will use that only
+//     then we will traverse from bottom-right to top-left and use distance of bottom and right neighbour
+    
+     vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        if (rows == 0) 
+            return matrix;
+        int cols = matrix[0].size();
+        vector<vector<int>> dist(rows, vector<int> (cols, INT_MAX - 100000));
+
+        //First pass: check for left and top
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    dist[i][j] = 0;
+                } else {
+                    if (i > 0)
+                        dist[i][j] = min(dist[i][j], dist[i - 1][j] + 1);
+                    if (j > 0)
+                        dist[i][j] = min(dist[i][j], dist[i][j - 1] + 1);
                 }
             }
         }
-        
-        int d[] = {-1, 0, 1, 0, -1};
-        while (!q.empty()) {
-            auto p = q.front();
-            q.pop();
-            int i=p.first, j=p.second;
-            for (int k=0; k<4; k++) {
-                int x = i+d[k], y = j+d[k+1];
-                if (x>=0 && y>=0 && x<n && y<m) {
-                    if (ans[x][y]>ans[i][j]+1) {
-                        ans[x][y] = ans[i][j]+1;
-                        q.push({x, y});
-                    }
-                }
+
+        //Second pass: check for bottom and right
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 0; j--) {
+                if (i < rows - 1)
+                    dist[i][j] = min(dist[i][j], dist[i + 1][j] + 1);
+                if (j < cols - 1)
+                    dist[i][j] = min(dist[i][j], dist[i][j + 1] + 1);
             }
         }
-        
-        return ans;
+        return dist;
     }
 };

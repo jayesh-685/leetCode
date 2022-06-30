878c97d8b6082bdfd1eed3eb13eeaad814e053d1
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-06-30 23:40:17 +0530

    Time: 22 ms (23.04%), Space: 10.8 MB (59.74%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,13 +1,20 @@
+// check notes
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
+        int n = nums.size();
+        //Sort the array
         sort(nums.begin(), nums.end());
-        int median = nums[nums.size()/2];
         
-        int cost=0;
-        for (auto x: nums)
-            cost += abs(x-median);
+        //Find the median
+        int median = n%2==0 ? (nums[n/2]+nums[n/2-1])/2 : nums[n/2];
+        int steps = 0;
         
-        return cost;
+        //Calculate steps
+        for(int num : nums) {
+            steps += abs(num-median);
+        }
+        
+        return steps;
     }
 };

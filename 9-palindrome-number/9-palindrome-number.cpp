f887c69062558a71cabbb8ac78d4b7a2e09df44c
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-07-04 15:29:23 +0530

    Time: 19 ms (64.34%), Space: 6 MB (11.50%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x<0)
+            return false;
+        
+        string s = to_string(x);
+        string temp = s;
+        reverse(s.begin(), s.end());
+        
+        return temp == s;
+    }
+};

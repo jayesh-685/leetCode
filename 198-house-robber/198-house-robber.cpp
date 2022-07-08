1b5c5ffd9ccdff25abb6a21a79c28b239c47ea0a
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-07-08 21:10:34 +0530

    Time: 3 ms (53.96%), Space: 7.6 MB (75.71%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        int sum[2][n];
+        int without=0, with=nums[0];
         
-        sum[0][0] = 0;  sum[1][0] = nums[0];
         
         for (int i=1; i<n; i++) {
-            sum[0][i] = max(sum[1][i-1], sum[0][i-1]);
-            sum[1][i] = sum[0][i-1] + nums[i];
+            int prevWithout = without;
+            without = max(without, with);
+            with = prevWithout + nums[i];
         }
         
-        return max(sum[0][n-1], sum[1][n-1]);
+        return max(with, without);
     }
 };

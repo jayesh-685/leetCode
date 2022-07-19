6acdd5c465afe0f4e216ca3e62b890bd74f4cfeb
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-07-19 14:36:03 +0530

    Time: 32 ms (50.65%), Space: 16.9 MB (85.96%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        if (nums.size() == 1)
-            return nums[0];
-        
-        int ans = nums[0]^nums[1];
-        
-        for (int i=2; i<nums.size(); i++)
-            ans ^= nums[i];
-        
-        return ans;
+        return accumulate(nums.begin(), nums.end(), 0, bit_xor<int>());
     }
 };

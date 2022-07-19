b337e14f7bd37bd90d9ba589579a81a7a6c952f5
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-07-19 16:13:07 +0530

    Time: 28 ms (63.35%), Space: 16.8 MB (85.96%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+// can do using finding freq of each element or sorting but this is better
+//     or find sum of all unique elements, double it, subtract from sum of original array
     int singleNumber(vector<int>& nums) {
         return accumulate(nums.begin(), nums.end(), 0, bit_xor<int>());
     }

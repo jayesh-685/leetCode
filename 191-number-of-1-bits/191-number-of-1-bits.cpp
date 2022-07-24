a2a45740aeb9b1afc5bbae84ffd8838243e10574
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-07-24 23:37:46 +0530

    Time: 3 ms (57.92%), Space: 6 MB (47.68%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
-    /*
-    n & (n - 1) drops the lowest set bit. It's a neat little bit trick.
-
-Let's use n = 00101100 as an example. This binary representation has three 1s.
-
-If n = 00101100, then n - 1 = 00101011, so n & (n - 1) = 00101100 & 00101011 = 00101000. Count = 1.
-
-If n = 00101000, then n - 1 = 00100111, so n & (n - 1) = 00101000 & 00100111 = 00100000. Count = 2.
-
-If n = 00100000, then n - 1 = 00011111, so n & (n - 1) = 00100000 & 00011111 = 00000000. Count = 3.
-
-n is now zero, so the while loop ends, and the final count (the numbers of set bits) is returned.
-*/
     int hammingWeight(uint32_t n) {
-        int count=0;
-        while (n) {
-            n = n&(n-1);
-            count++;
-        }
-        
-        return count;
+        return __builtin_popcount(n);
     }
 };

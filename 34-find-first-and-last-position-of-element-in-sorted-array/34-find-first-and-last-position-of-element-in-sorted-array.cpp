898a0827f77c0f5fd0f05aac3b848b52023219d0
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-07-25 10:57:53 +0530

    Time: 7 ms (90.42%), Space: 13.7 MB (17.99%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,4 +1,62 @@
-class Solution {
+
+/*
+public int[] searchRange(int[] a, int target){
+		
+		int[] result = {-1, -1};
+		
+		if (a == null || a.length == 0)
+			return result;
+		
+		result[0] = findStartPosition(a, target);
+		result[1] = findEndPosition(a, target);
+		
+		return result;
+}
+
+public int findStartPosition(int[] a, int target){
+
+	int left = 0;
+	int right = a.length-1;
+	int start =-1;
+	
+	while(left<= right){
+	
+		int mid = left+(right-left)/2;
+		
+		if (a[mid] == target){
+				start = mid; // this is start
+				right = mid-1; // lets see if there one more on the left
+		}else if (target > a[mid]){
+			left = mid+1;
+		}else{
+			right = mid-1;
+		}
+	}
+
+	return start;
+}
+
+public int findEndPosition(int[] a, int target){
+	int left = 0;
+	int right = a.length-1;
+	int end = -1;
+	
+	while(left <= right){
+		int mid = left + (right-left)/2;
+		
+		if (a[mid] == target){
+			end = mid;	  // this is the end
+			left = mid+1; // lets see if there is one more on the right
+		}else if (target > a[mid])
+			left = mid +1;
+		else 
+			right = mid -1;
+	
+	}
+	
+	return end;
+}
+*/class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector <int> ans;

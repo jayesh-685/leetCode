e39988f2ad8d36ccdb6c6d8f8e2b9db6d6b5d5e1
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-07-27 00:08:33 +0530

    Time: 5 ms (10.14%), Space: 6 MB (26.95%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
 //     if any one of the bits are 1 then the result is 1 and if both are 0 or 1 then the result is 0 (with a carry in case of both 1) so the output resembles the xor operation except the carry part. For the carry part we take & of both and left shift by 1 since we will adding carry to the left bit and then add them again using xor. Repeat till carry is not 0.
     int getSum(int a, int b) {
-int ans = a ^ b;
-unsigned int c = a & b;
-while(c != 0) {
-c <<= 1;
-int ans_prim = ans ^ c;
-c = ans & c;
-ans = ans_prim;
-}
-return ans;
+        int ans = a ^ b;
+        unsigned int c = a & b;
+        while(c != 0) {
+        c <<= 1;
+        int ans_prim = ans ^ c;
+        c = ans & c;
+        ans = ans_prim;
+        }
+        return ans;
     }
 };

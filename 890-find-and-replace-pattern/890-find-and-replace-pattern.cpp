6ae487d4bd7ec07f6186b54c7d6bee26ce637beb
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-07-29 10:56:23 +0530

    Time: 7 ms (55.39%), Space: 8.7 MB (12.79%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+//     use 2 maps or 1 map and 1 array
+    bool match (string s, string pattern) {
+        unordered_map <char, char> m1;
+        unordered_map <char, char> m2;
+        for (int i=0; i<s.length(); i++) {
+            char c=s[i], p=pattern[i];
+            if (m1.find(c) == m1.end()) {
+                if (m2.find(p) != m2.end())
+                    return false;
+                m1[c] = p; m2[p] = c;
+            } else {
+                if (m1[c]!=p || m2[p]!=c)
+                    return false;
+            }
+        }
+        return true;
+    }
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        vector <string> ans;
+        for (auto s: words)
+            if (match(s, pattern))
+                ans.push_back(s);
+                
+        return ans;
+    }
+};

b0a8592cb0f68455ef15d3d1e713e22cbc95a8a5
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-08-07 12:34:27 +0530

    Time: 181 ms (44.58%), Space: 67.7 MB (90.81%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//     if after adding the curr no the currSum becomes smaller, we don't need the previous sub array so we set currSum to currElement
     int maxSubArray(vector<int>& nums) {
         int maxSum, currSum;
         maxSum = currSum = nums[0];

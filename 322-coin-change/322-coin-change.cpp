3b30521b41e927d3ec913ab58e8831a15df304fc
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-08-17 00:07:38 +0530

    Time: 162 ms (42.29%), Space: 21.5 MB (32.59%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -6,7 +6,7 @@ public:
         if (rem<0)  return -1;
         if (rem==0) return 0;
         
-        if (dp[rem-1] != 0) return dp[rem-1];
+        if (dp[rem] != 0) return dp[rem];
         
         int ans = INT_MAX;
         for (auto coin: coins) {
@@ -15,11 +15,11 @@ public:
                 ans = res+1;
         }
         
-        return dp[rem-1] = (ans==INT_MAX ? -1 : ans);
+        return dp[rem] = (ans==INT_MAX ? -1 : ans);
     }
     int coinChange(vector<int>& coins, int amount) {
         if(amount<1) return 0;
-        vector <int> temp (amount);
+        vector <int> temp (amount+1);
         dp = temp;
         return solve(coins, amount);
     }

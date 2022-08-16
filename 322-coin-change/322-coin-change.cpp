5e11e77af7780fec3624408145b6e1754fa7d743
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-08-17 00:51:58 +0530

    Time: 139 ms (55.76%), Space: 14.5 MB (54.56%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
+//     start from amount=0 and go till amount=amount
+//     for each amount iterate over all the coins and if coin is less than amount update value of dp[amount]
+//     ex if amount is 5 and coin value is 2 then minimum no of coins required for amount 5 is minimum no of coins required for amount 3 (5-2) + one 2 ruppee coin.
     int coinChange(vector<int>& coins, int amount) {
+//         can initialize with amount+1 or infinity
         vector <int> dp (amount+1, amount+1);
-        dp[0] = 0;
+        dp[0] = 0;  // no coins for no amount
         
         for (int i=1; i<=amount; i++) {
             for (auto coin: coins) {

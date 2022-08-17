fa30d7aba7ffdee5762e522162080bcb73a19818
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-08-17 10:59:42 +0530

    Time: 6 ms (80.34%), Space: 6.2 MB (84.63%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
-    // bool isPalindrome (string s) {
-    //     for (int i=0; i<s.length()/2; i++) {
-    //         if (s[i] != s[s.length()-i-1])
-    //             return false;
-    //     }
-    //     return true;
-    // }
-    
+    int count;
     string str;
-    vector <vector <int>> dp;
-    
-    bool isPalindrome (int i, int j) {
-        if (i>=j)
-            return true;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        if (str[i]==str[j] && isPalindrome(i+1, j-1))
-            return dp[i][j]=true;
-        return dp[i][j]=false;
+    void solve (int i, int j) {
+        if (i<0 || j>str.length() || str[i]!=str[j])
+            return;
+        count++;
+        solve(i-1, j+1);
     }
     int countSubstrings(string s) {
-        int ans=0;
-        str=s;
-        vector <vector <int>> temp (s.length()+1, vector<int>(s.length()+1, -1));
-        dp = temp;
-        for (int i=1; i<=s.length(); i++) {
-            for (int j=0; j<i; j++) {
-                if (isPalindrome(j, i-1))
-                    ans++;
-            }
+        count=0; str=s;
+        for (int i=0; i<s.length(); i++) {
+            solve(i, i); solve(i, i+1);
         }
         
-        return ans;
+        return count;
     }
 };

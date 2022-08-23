2d256c1e580f75b298797c633946d1d57a60f148
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-08-24 00:36:58 +0530

    Time: 49 ms (28.10%), Space: 30.1 MB (5.26%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
@@ -1,31 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void serialize (TreeNode* root, string& s) {
+    void ser(TreeNode *root, string& s) {
         if (!root) {
-            s += ",N,";
+            s += '$';
             return;
         }
         
-        s += ","+to_string(root->val)+",";
-        serialize(root->left, s);
-        serialize(root->right, s);
+        s += '&' + to_string(root->val) + '&';
+        ser(root->left, s);
+        ser(root->right, s);
     }
-    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        string s1, s2;
-        serialize(root, s1); serialize(subRoot, s2);
-        cout << s1 << endl;
-        cout << s2 << endl;
-        return s1.find(s2) != string::npos;
+    
+    bool isSubtree(TreeNode* s, TreeNode* t) {
+        string s1;
+        string s2;
+        ser(s, s1);
+        ser(t, s2);
+        
+        return s1.find(s2) != std::string::npos;
     }
 };

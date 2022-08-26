cd08320293c09e83d8084ea1a134db54766e70bd
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-08-26 09:54:24 +0530

    Time: 30 ms (58.26%), Space: 18 MB (64.62%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
+//     xor of any no with itself is 0 so start with initial value equal to array size and xor with index and value
     int missingNumber(vector<int>& nums) {
-        int n = nums.size()+1;
+        int res=nums.size();
+        for (int i=0; i<nums.size(); i++) {
+            res ^= i;
+            res ^= nums[i];
+        }
         
-        return abs((n*(n-1)/2)-accumulate(nums.begin(), nums.end(), 0));
+        return res;
     }
 };

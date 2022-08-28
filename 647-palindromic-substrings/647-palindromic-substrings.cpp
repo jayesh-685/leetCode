3849208f610545663d30023548a9925c0d998922
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-08-28 14:18:18 +0530

    Time: 3 ms (93.76%), Space: 6.2 MB (84.46%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-//     similar to longest palindrome
-//     for each char find all odd length palindromes and even length palindromes
-    int count;
     string str;
-    void solve (int i, int j) {
-        if (i<0 || j>str.length() || str[i]!=str[j])
+    void solve (int i, int j, int& count) {
+        if (i<0 || j>=str.length() || str[i]!=str[j])
             return;
+        
         count++;
-        solve(i-1, j+1);
+        solve(i-1, j+1, count);
     }
     int countSubstrings(string s) {
-        count=0; str=s;
+        str = s;
+        int ans=0;
+        
         for (int i=0; i<s.length(); i++) {
-            solve(i, i); solve(i, i+1);
+            int count=0;
+            solve(i, i, count); solve(i, i+1, count);
+            ans += count;
         }
         
-        return count;
+        return ans;
     }
 };

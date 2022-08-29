5a0c8ab2d047d963cc828477ad04cadb71184192
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-08-30 00:43:29 +0530

    Time: 213 ms (45.93%), Space: 93.2 MB (89.40%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    // use a variable to store minimum value so far
     int maxProfit(vector<int>& prices) {
-        int minSoFar = prices[0], maxProfit = 0, profit;
+        int ans=0, minEl=prices[0];
         for (int i=1; i<prices.size(); i++) {
-            profit = prices[i]-minSoFar;
-            maxProfit = max(maxProfit, profit);
-            minSoFar = min(minSoFar, prices[i]);
+            if (prices[i] >= minEl)
+                ans = max(ans, prices[i]-minEl);
+            else
+                minEl = prices[i];
         }
         
-        return maxProfit;
+        return ans;
     }
 };

81e46428ab70b29fea920e231db5bd44dd1f28f0
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-08-29 10:23:26 +0530

    Time: 107 ms (76.45%), Space: 11.1 MB (82.16%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-//     start from amount=0 and go till amount=amount
-//     for each amount iterate over all the coins and if coin is less than amount update value of dp[amount]
-//     ex if amount is 5 and coin value is 2 then minimum no of coins required for amount 5 is minimum no of coins required for amount 3 (5-2) + one 2 ruppee coin.
-    int coinChange(vector<int>& coins, int amount) {
-//         can initialize with amount+1 or infinity
-        vector <int> dp (amount+1, amount+1);
-        dp[0] = 0;  // no coins for no amount
+    int memo[10001];
+    int solve (vector<int>& coins, int amount) {
+        if (amount<0)
+            return INT_MAX;
+        if (amount==0)
+            return 0;
         
-        for (int i=1; i<=amount; i++) {
-            for (auto coin: coins) {
-                if (coin <= i) {
-                    dp[i] = min(dp[i], 1+dp[i-coin]);
-                }
+        if (memo[amount] != 0)
+            return memo[amount];
+        
+        int ans=INT_MAX;
+        
+        for (int i=0; i<coins.size(); i++) {
+            int res = coinChange(coins, amount-coins[i]);
+            if (res!=INT_MAX && res!=-1) {
+                ans = min(ans, 1+res);
             }
         }
         
-        return dp[amount]==amount+1? -1 : dp[amount];
+        return memo[amount] = ans==INT_MAX ? -1 : ans;
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        return solve(coins, amount);
     }
 };

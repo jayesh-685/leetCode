c068761fa4541aafd4ecf9782a7701149a65d264
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-08-30 20:24:23 +0530

    Time: 7 ms (54.08%), Space: 11.1 MB (29.48%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    
+//     even though the array is rotated, atleast one half of the array is still sorted so check which half is sorted and check if our target lies in that half or the other
     int search(vector<int>& nums, int target) {
         int l=0, r=nums.size()-1;
         
@@ -10,14 +12,18 @@ public:
             
             // if left half is sorted
             if (nums[l] <= nums[m]) {
+//                 if element lies in left half
                 if (target>=nums[l] && target<nums[m]) {
                     r=m-1;
+//                     else it lies in right half
                 } else {
                     l=m+1;
                 }
-            } else {
+            } else {  // if right half is sorted
+//                 if element lies in right half
                 if (target>nums[m] && target<=nums[r]) {
                     l=m+1;
+//                     else it lies in left half
                 } else {
                     r=m-1;
                 }

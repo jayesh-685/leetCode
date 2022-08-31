3525a3b0005825fe8d195f16c5ab08ce4fe0b728
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-08-31 16:22:11 +0530

    Time: 13 ms (7.46%), Space: 10.1 MB (95.43%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+//     if first elements < last element then array is not rotated so return the first element
+//     if array is rotated then minimum element lies in that half which is not sorted so find that apply b.s on that half
     int findMin(vector<int>& nums) {
         int n=nums.size();
         if (nums[0]<nums[n-1])

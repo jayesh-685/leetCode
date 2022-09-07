c1262f503643e7cd24195324861b52d5053362de
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-09-07 20:10:27 +0530

    Time: 1115 ms (5.34%), Space: 336.6 MB (5.06%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool solve (int i, vector<int>& nums, int set1, int sum, vector<vector<int>>& dp) {
+        
+        if (i==nums.size())
+            return (double)set1==(double)sum/2;
+        
+        if (dp[i][set1]!=-1)
+            return dp[i][set1];
+                    
+        return dp[i][set1]=solve(i+1, nums, set1+nums[i], sum+nums[i], dp) || solve(i+1, nums, set1, sum+nums[i], dp);
+    }
+    
+    bool canPartition(vector<int>& nums) {
+        
+        int n=nums.size();
+        vector<vector<int>> dp (n, vector<int>(20000, -1));
+        return solve(0, nums, 0, 0, dp);
+    }
+};

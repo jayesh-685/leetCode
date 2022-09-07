b7249dcba36c5037397923fdbd65fd8489c99b81
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-09-07 23:04:13 +0530

    Time: 1933 ms (5.00%), Space: 223.1 MB (5.06%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    bool solve (int i, vector<int>& nums, int target, vector<vector<int>>& dp) {
-        
-        if (i==nums.size())
-            return !target;
-        
-        if (target<0)
-            return false;
-        
-        if (dp[i][target]!=-1)
-            return dp[i][target];
-                    
-        return dp[i][target]=solve(i+1, nums, target-nums[i], dp) || solve(i+1, nums, target, dp);
-    }
-    
     bool canPartition(vector<int>& nums) {
+        unordered_set <int> st;
+        st.insert(0);
+        int n=nums.size(), sum=accumulate(nums.begin(), nums.end(), 0);
         
-        int n=nums.size();
-        int sum=accumulate(nums.begin(), nums.end(), 0);
         if (sum%2)
             return false;
         
-        vector<vector<int>> dp (n, vector<int>(sum/2+1, -1));
-        return solve(0, nums, sum/2, dp);
+        sum /= 2;
+        
+        for (int i=0; i<n; i++) {
+            unordered_set <int> temp;
+            for (auto it=st.begin(); it!=st.end(); it++) {
+                if (*it==sum || (*it+nums[i]==sum))
+                    return true;
+                
+                temp.insert(*it);
+                temp.insert(*it + nums[i]);
+            }
+            
+            st = temp;
+        }
+        
+        return false;
     }
 };

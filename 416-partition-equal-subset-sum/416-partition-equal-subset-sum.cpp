c87fc7c6def4888218f3e326e1ad2ce6f9c2d8a5
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-09-08 16:52:31 +0530

    Time: 724 ms (11.62%), Space: 12.9 MB (56.97%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int target = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            target += nums[i];
-        }
-        if (target % 2 != 0) {
-            return false;
-        }
-        target /= 2;
+        int k = accumulate(nums.begin(), nums.end(), 0), n=nums.size();;
+        if (k%2)   return false;
+        k /= 2;
         
-        unordered_set<int> dp;
-        dp.insert(0);
+        vector<vector<bool>> dp (n+1, vector<bool>(k+1));
         
-        for (int i = 0; i < nums.size(); i++) {
-            unordered_set<int> dpNext;
-            for (auto it = dp.begin(); it != dp.end(); it++) {
-                if (*it + nums[i] == target) {
-                    return true;
-                }
-                dpNext.insert(*it + nums[i]);
-                dpNext.insert(*it);
+        for (int i=0; i<=n; i++)
+            dp[i][0]=true;
+        for (int i=1; i<=k; i++)
+            dp[0][i]=false;
+        
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=k; j++) {
+                if (j-nums[i-1]>=0)
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
             }
-            dp = dpNext;
         }
         
-        return false;
+        return dp[n][k];
     }
 };

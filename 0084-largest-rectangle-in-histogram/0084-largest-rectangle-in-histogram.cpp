625df8de03a8916569e308849f2324541d0f9ac5
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-11-12 17:50:47 +0530

    Time: 180 ms (95.70%), Space: 75.3 MB (91.48%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,34 +1,16 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-        vector<int> left(n);
-        stack <int> st1;
+        int ans=0, n=heights.size();
+        stack <int> st;
         
-        for (int i=0; i<n; i++) {
-            while (!st1.empty() && heights[st1.top()]>=heights[i])
-                st1.pop();
-            left[i] = st1.empty()? -1 : st1.top();
-            st1.push(i);
-        }
-        
-        vector<int> right(n);
-        stack <int> st2;
-        
-        for (int i=n-1; i>=0; i--) {
-            while (!st2.empty() && heights[st2.top()]>=heights[i])
-                st2.pop();
-            right[i] = st2.empty()? -1 : st2.top();
-            st2.push(i);
-        }
-        
-        int ans=0;
-        
-        for (int i=0; i<n; i++) {
-            int l=left[i], r=right[i];
-            if (r==-1)  r=n;
-            
-            ans = max(ans, (r-l-1)*heights[i]);
+        for (int i=0; i<=n; i++) {
+            while (!st.empty() && (i==n || heights[st.top()] >= heights[i])) {
+                int height = heights[st.top()]; st.pop();
+                int width = st.empty() ? i : i-st.top()-1;
+                ans = max(ans, height*width);
+            }
+            st.push(i);
         }
         
         return ans;

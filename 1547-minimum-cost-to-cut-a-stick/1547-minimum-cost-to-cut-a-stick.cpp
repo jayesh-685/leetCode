d60834ff5a5ba8437f72f0c7643830712a3a323f
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-12-01 01:18:13 +0530

    Time: 66 ms (91.00%), Space: 10.4 MB (42.08%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -21,7 +21,22 @@ public:
         cuts.insert(cuts.begin(), 0);
         sort(cuts.begin(), cuts.end());
         
-        vector<vector<int>> dp (c+1, vector<int>(c+1, -1));
-        return solve(cuts, 1, c, dp);
+        vector<vector<int>> dp (c+2, vector<int>(c+2, 0));
+        
+        for (int i=c; i>=1; i--) {
+            for (int j=1; j<=c; j++) {
+                if (i<=j) {
+                    int mini=INT_MAX;
+                    for (int k=i; k<=j; k++) {
+                        int ans = cuts[j+1]-cuts[i-1] + dp[i][k-1] + dp[k+1][j];
+                        mini = min(mini, ans);
+                    }
+
+                    dp[i][j]=mini;
+                }
+            }
+        }
+        
+        return dp[1][c];
     }
 };

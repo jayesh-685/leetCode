74308aa08633ab1b00fec5673edc3a0a7264c70a
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2023-01-22 14:50:25 +0530

    Time: 99 ms (94.46%), Space: 49.3 MB (80.30%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    vector<vector<string>> ans;
-    bool isPalindrome (string s) {
-        string t = s;
-        reverse(begin(s), end(s));
-        return t==s;
+    vector<vector<string>> partition(string s) {
+        int len = s.length();
+        vector<vector<bool>> dp (len, vector <bool> (len, false));
+        vector<vector<string>> result;
+        vector<string> currentList;
+        dfs(result, s, 0, currentList, dp);
+        return result;
     }
-    void solve (string& s, int st, int en, vector<string>& v) {
-        if (st==size(s)) {
-            ans.push_back(v);
-            return;
-        }
-        string temp="";
-        for (int i=st; i<=en; i++) {
-            temp += s[i];
-            if (isPalindrome(temp)) {
-                v.push_back(temp);
-                solve(s, i+1, en, v);
-                v.pop_back();
+
+    void dfs(vector<vector<string>> &result, string &s, int start, vector<string> &currentList, vector<vector<bool>> &dp) {
+        if (start >= s.length()) result.push_back(currentList);
+        for (int end = start; end < s.length(); end++) {
+            if (s[start] == s[end] && (end - start <= 2 || dp[start + 1][end - 1])) {
+                dp[start][end] = true;
+                currentList.push_back(s.substr(start, end - start + 1));
+                dfs(result, s, end + 1, currentList, dp);
+                currentList.pop_back();
             }
         }
-    }
-    vector<vector<string>> partition(string s) {
-        int n=size(s);
-        vector<string> v;
-        solve(s, 0, n-1, v);
-
-        return ans;
     }
 };

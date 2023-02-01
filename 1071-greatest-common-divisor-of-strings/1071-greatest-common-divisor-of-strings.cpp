5f61d048177ee3e2347a8ce477c8f45c6a8becad
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2023-02-02 00:39:38 +0530

    Time: 3 ms (83.39%), Space: 7.2 MB (43.04%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,27 +1,14 @@
 class Solution {
-    public:
-    bool valid(string str1, string str2, int k) {
-        int len1 = str1.length(), len2 = str2.length();
-        if (len1 % k > 0 || len2 % k > 0) {
-            return false;
-        } else {
-            string base = str1.substr(0, k);
-            int n1=str1.length()/k, n2=str2.length()/k;
-            string s1="", s2="";
-            for (int i=0; i<n1; i++)    s1 += base;
-            for (int i=0; i<n2; i++)    s2 += base;
-            return s1==str1 && s2==str2;
-        }
-    }
-    
+public:
     
     string gcdOfStrings(string str1, string str2) {
-        int len1 = str1.length(), len2 = str2.length();
-        for (int i = min(len1, len2); i >= 1; --i) {
-            if (valid(str1, str2, i)) {
-                return str1.substr(0, i);
-            }
+        // Check if they have non-zero GCD string.
+        if (str1 + str2 != str2 + str1) {
+            return "";
         }
-        return "";
+
+        // Get the GCD of the two lengths.
+        int gcdLength = gcd(str1.size(), str2.size());
+        return str1.substr(0, gcdLength);
     }
 };

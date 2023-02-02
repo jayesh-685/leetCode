7293fb6aa6b266218443cb004381901a6cba2fbb
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2023-02-03 02:01:51 +0530

    Added solution - LeetHub

---
+++ Word Ladder I - GFG/word-ladder-i.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+// if we can develop a graph with the words as nodes then we just have to find the shortest path from startWord
+// to target word (using bfs)
+// to find the neighbours of each word, just replace each char of word with a to z and see if it exists
+    int wordLadderLength(string startWord, string targetWord, vector<string>& wordList) {
+        // Code here
+        
+        unordered_set <string> st (begin(wordList), end(wordList));  // to check if the new word we have created exists or not
+        queue < pair<string,int> > q; // for bfs
+        q.push({startWord, 1});
+        st.erase(startWord);  // any word that we find, we will erase from set so that we don't visit it again since we are finding the shortest path
+        
+        while (!q.empty()) {
+            string word = q.front().first;
+            int dist = q.front().second;
+            q.pop();
+            
+            if (word == targetWord) return dist;
+            
+            for (int i=0; i<word.length(); i++) {
+                string temp=word;
+                for (char ch='a'; ch<='z'; ch++) {
+                    word[i]=ch;
+                    if (st.find(word) != end(st)) {
+                        st.erase(word);
+                        q.push({word, dist+1});
+                    }
+                }
+                word = temp;
+            }
+        }
+        
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<string>wordList(n);
+		for(int i = 0; i < n; i++)cin >> wordList[i];
+		string startWord, targetWord;
+		cin >> startWord >> targetWord;
+		Solution obj;
+		int ans = obj.wordLadderLength(startWord, targetWord, wordList);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends

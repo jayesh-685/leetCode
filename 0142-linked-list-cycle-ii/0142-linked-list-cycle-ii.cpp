4de40baf90f8909714043370accf38fd8cc06792
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2023-03-09 11:43:42 +0530

    Time: 6 ms (90.60%), Space: 7.6 MB (68.59%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/*
+2.1) L1 is defined as the distance between the head point and entry point
+
+2.2) L2 is defined as the distance between the entry point and the meeting point
+
+2.3) C is defined as the length of the cycle
+
+2.4) n is defined as the travel times of the fast pointer around the cycle When the first encounter of the slow pointer and the fast pointer
+
+According to the definition of L1, L2 and C, we can obtain:
+
+the total distance of the slow pointer traveled when encounter is L1 + L2
+
+the total distance of the fast pointer traveled when encounter is L1 + L2 + n * C
+
+Because the total distance the fast pointer traveled is twice as the slow pointer, Thus:
+
+2 * (L1+L2) = L1 + L2 + n * C => L1 + L2 = n * C => L1 = (n - 1) C + (C - L2)*
+
+It can be concluded that the distance between the head location and entry location is equal to the distance between the meeting location and the entry location along the direction of forward movement.
+
+So, when the slow pointer and the fast pointer encounter in the cycle, we can define a pointer "entry" that point to the head, this "entry" pointer moves one step each time so as the slow pointer. When this "entry" pointer and the slow pointer both point to the same location, this location is the node where the cycle begins.
+*/
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+    if (head == NULL || head->next == NULL)
+        return NULL;
+    
+    ListNode *slow  = head;
+    ListNode *fast  = head;
+    ListNode *entry = head;
+    
+    while (fast->next && fast->next->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {                      // there is a cycle
+            while(slow != entry) {               // found the entry location
+                slow  = slow->next;
+                entry = entry->next;
+            }
+            return entry;
+        }
+    }
+    return NULL;                                 // there has no cycle
+}
+};

ee3d7601e36e8bb767c1ee79a22b2bdeaf112e87
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2023-04-09 23:18:01 +0530

    Time: 608 ms (76.92%), Space: 170.1 MB (63.25%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
@@ -1,52 +1,46 @@
 class Solution {
 public:
-    // it's a directed graph so you can't start dfs from any arbitray node, you need to consider all paths from all nodes which will be O(nm)*n. 
-    // we will be storing count for all colors for all nodes so that we don't need to recompute it again and again.
-    // for ex if there is edge from 2-3 and 2-4 and if we know count for 3 and 4 we will take max of those and add 1 to the color of 2.
-    int dfs(int node, string& colors, vector<vector<int>>& adj, vector<vector<int>>& count,
-            vector<bool>& visit, vector<bool>& inStack) {
-        // If the node is already in the stack, we have a cycle.
-        if (inStack[node]) {
-            return INT_MAX;
-        }
-        if (visit[node]) {
-            return 0;
-        }
-        // Mark the current node as visited and part of current recursion stack.
-        visit[node] = true;
-        inStack[node] = true;
-
-        for (auto& neighbor : adj[node]) {
-            if (dfs(neighbor, colors, adj, count, visit, inStack) == INT_MAX) {
-                return INT_MAX;
-            }
-            for (int i = 0; i < 26; i++) {
-                count[node][i] = max(count[node][i], count[neighbor][i]);
-            }
-        }
-
-        // After all the incoming edges to the node are processed,
-        // we count the color on the node itself.
-        count[node][colors[node] - 'a']++;
-        // Remove the node from the stack.
-        inStack[node] = false;
-        return *max_element(begin(count[node]), end(count[node]));
-    }
-
     int largestPathValue(string colors, vector<vector<int>>& edges) {
         int n = colors.length();
         vector<vector<int>> adj(n);
+        vector<int> indegree(n);
+
         for (auto& edge : edges) {
             adj[edge[0]].push_back(edge[1]);
+            indegree[edge[1]]++;
         }
 
         vector<vector<int>> count(n, vector<int>(26));
-        vector<bool> visit(n), inStack(n);
-        int answer = 0;
+        queue<int> q;
+
+        // Push all the nodes with indegree zero in the queue.
         for (int i = 0; i < n; i++) {
-            answer = max(answer, dfs(i, colors, adj, count, visit, inStack));
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
         }
 
-        return answer == INT_MAX ? -1 : answer;
+        int answer = 0, nodesSeen = 0;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            answer = max(answer, ++count[node][colors[node] - 'a']);
+            nodesSeen++;
+
+            for (auto& neighbor : adj[node]) {
+                for (int i = 0; i < 26; i++) {
+                    // Try to update the frequency of colors for neighbor to include paths
+                    // that use node->neighbor edge.
+                    count[neighbor][i] = max(count[neighbor][i], count[node][i]);
+                }
+
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.push(neighbor);
+                }
+            }
+        }
+
+        return nodesSeen < n ? -1 : answer;
     }
 };

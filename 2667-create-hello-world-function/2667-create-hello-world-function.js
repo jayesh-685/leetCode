5cf5efadb77a8c704c296117693f1ac1f9160cb4
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2023-05-06 01:24:11 +0530

    Time: 65 ms (100.00%), Space: 41.5 MB (100.00%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+
+var createHelloWorld = function() {
+    return function() {
+        return "Hello World";
+    }
+};

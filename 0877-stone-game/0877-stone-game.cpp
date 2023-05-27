8e8b12d036f8af5e3d4725132cbe5d0e403dcafb
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2023-05-27 22:39:23 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (62.10%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    // since alice is starting first alice can choose all the odd indexed stones or all the even indexed stones and since the sum is even sum of odd indexed stones is always less than or greater than the even indexd stones so alice can choose whichever is higher.
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
